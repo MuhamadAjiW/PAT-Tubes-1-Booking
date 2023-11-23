91b2ca5e691ec1802391cd404278865c94e9ba8e
Vanessa Rebecca <64979714+vanessrw@users.noreply.github.com>
2023-11-23 19:02:57 +0700

    fix: port

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
-const App = Bun.serve({
-    port: 3000,
-    fetch(request){
-        return new Response("Server has started!");
-    },
-})
+// tugas-besar-pat-booking/src/App.tsx
 
-console.log("Server started at port 3000")
+import express from 'express';
+import cors from 'cors';
+import { pool } from './connection';
+
+const app = express();
+const port = 3100;
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/book-kursi', async (req, res) => {
+  const { acaraId, kursiId, userId } = req.body;
+  res.json({ success: true, message: 'Booking successful' });
+});
+
+app.listen(port, () => {
+  console.log(`Booking API listening at http://localhost:${port}`);
+});
+
+// Bun setup
+const App = Bun.serve({
+  port: 3101,
+  async fetch(request: Request) {
+    const url = `http://localhost:${port}${request.url}`;
+    
+    const newRequest = new Request(url, {
+      method: request.method,
+      headers: request.headers,
+      body: request.body,
+    });
+
+    return fetch(newRequest);
+  },
+});
+
+console.log("Server started at port 3101");
