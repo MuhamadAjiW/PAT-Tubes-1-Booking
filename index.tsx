a5fe62b5b0082fe3ef942f7846c5f911c80dcc3d
MuhamadAjiW <16521119@mahasiswa.itb.ac.id>
2023-11-26 16:25:58 +0700

    feat: url signing

--- index.tsx
+++ index.tsx
@@ -1,16 +1,8 @@
 import { App } from "./src/app";
-// import { PDFUtils } from "./src/utils/pdf-utils";
+import { SignatureUtil } from "./src/utils/signature-utils";
 
-const app = new App;
-app.run();
+// const app = new App;
+// app.run();
 
-// PDFUtils.generateBookingSuccess("a.pdf", 
-//     {
-//         email: "test@gmail.com",
-//         namaAcara: "tai",
-//         kursiId: 1,
-//         invoiceNumber: "255225",
-//         bookingId: 5,
-//         failureReason: "TAI LU ANJINGG"
-//     }
-// );
+console.log(SignatureUtil.generateSignature("test", SignatureUtil.PDFExpiry));
+SignatureUtil.verifySignature("V33tMcTqq29FNeDxlQtd41wColf1lZ_AjsaDW_495ens2-LzgNvH_QCPNGTMtznaLZ8Gcy24kgJCxMGf956_YA");
--- src/utils/signature-utils.tsx
+++ src/utils/signature-utils.tsx
@@ -1,24 +1,74 @@
 import * as crypto from 'crypto';
+import moment from 'moment';
 
 export class SignatureUtil{
     // TODO: Implement
-    public static generateSignature(identifier: string){
+    public static PDFExpiry: number = 300 * 1000 // in milis
+    public static secretKey: string = "signedsignedsignedsignedsignedee";
+    public static dateFormat = "YYYY-MM-DDTHH-mm-ss.SSS";
 
+    public static generateSignature(identifier: string, expiry: number): string | null{
+        try{
+            const expiration: number = Date.now() + expiry;
+            const formattedTimestamp: string = moment(expiration).format(this.dateFormat)
+            const salt: string = crypto.randomBytes(10).toString('hex');
+            return this.encryptSignature(identifier + "|" + formattedTimestamp + "|" + salt);
+        } catch (error){
+            console.error(error);
+            return null;
+        }
     }
 
-    public static verifySignature(signature: string){
+    public static verifySignature(signature: string): boolean{
+        const decryptedSignature = this.decryptSignature(signature);
+        const parts = decryptedSignature.split("|");
+        console.log(decryptedSignature);
 
+        if (parts.length == 3){
+            console.log(parts[0])
+            const expirationTime = moment(parts[1], this.dateFormat);
+            console.log(parts[1]);
+            console.log(expirationTime.isAfter(moment()));
+            return expirationTime.isAfter(moment());
+        } else {
+            return false;
+        }
     }
 
     public static getIdentifier(signature: string){
+        const decryptedSignature = this.decryptSignature(signature);
+        const parts = decryptedSignature.split("|");
+        console.log(decryptedSignature);
 
+        if (parts.length == 3){
+            console.log(parts[0])
+            return parts[0];
+        } else {
+            return null;
+        }
     }
 
-    private static encryptSignature(){
-
+    private static encryptSignature(signature: string): string{
+        try {
+            const cipher = crypto.createCipher('aes-256-cbc', this.secretKey)
+            let encryptedBytes = cipher.update(signature, 'utf-8', 'base64url');
+            encryptedBytes += cipher.final('base64url');
+            return encryptedBytes;
+        } catch (error) {
+            console.error(error)
+            throw error;
+        }
     }
 
-    private static decryptSignature(){
-
+    private static decryptSignature(signature: string): string{
+        try {
+            const cipher = crypto.createDecipher('aes-256-cbc', this.secretKey)
+            let decryptedBytes = cipher.update(signature, 'base64url', 'utf-8');
+            decryptedBytes += cipher.final('utf-8');
+            return decryptedBytes;
+        } catch (error) {
+            console.error(error)
+            throw error;
+        }
     }
 }
